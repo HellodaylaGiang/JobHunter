135aa5ae846842356967cb3de52b60ac6d9d048b
Bún Đậu <124412911+HellodaylaGiang@users.noreply.github.com>
2024-07-04 19:59:53 +0700

    4/7 part 3

--- src/main/java/vn/hoidanit/jobhunter/domain/Resume.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Resume.java
@@ -2,18 +2,18 @@ package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
@@ -24,13 +24,14 @@ import vn.hoidanit.jobhunter.util.constant.ResumeStateEnum;
 @Getter
 @Setter
 public class Resume {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank(message = "email không được để trống")
     private String email;
 
+    @NotBlank(message = "url không được để trống (upload cv chưa thành công)")
     private String url;
 
     @Enumerated(EnumType.STRING)
