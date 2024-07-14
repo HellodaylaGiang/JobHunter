2562a651ea214c545be213451e2cf94410b62826
Bún Đậu <124412911+HellodaylaGiang@users.noreply.github.com>
2024-07-14 17:47:47 +0700

    14/7 part 5

--- src/main/java/vn/hoidanit/jobhunter/controller/SubscriberController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/SubscriberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.Subscriber;
 import vn.hoidanit.jobhunter.service.SubscriberService;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
 import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
 import vn.hoidanit.jobhunter.util.error.IdInvalidException;
 
@@ -46,6 +47,16 @@ public class SubscriberController {
             throw new IdInvalidException("Id " + subsRequest.getId() + " không tồn tại");
         }
         return ResponseEntity.ok().body(this.subscriberService.update(subDB, subsRequest));
+    }
+
+    @PostMapping("/subscribers/skills")
+    @ApiMessage("Get subscriber a skill")
+    public ResponseEntity<Subscriber> getSubscriber() throws IdInvalidException {
+
+        String email = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+        return ResponseEntity.ok().body(this.subscriberService.findByEmail(email));
     }
 
 }
